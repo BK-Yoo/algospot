b71ebb4f455bc15f2a58b6adbb094f587a6b0b9e
BK-Yoo <osehyum@gmail.com>
2019-05-25 23:10:46 +0900

    dir clean, prob solve 1.29, 30 31

--- lisp/ch01.scm
+++ lisp/ch01.scm
@@ -98,3 +98,70 @@
   )
   (prac 2 1 0 n)
 )
+
+
+;1.29
+(define (cube x) (* x x x))
+(define (simpson f a b n)
+  (define (iter fk i n result)
+    (if (> i n)
+      result
+      (iter fk (+ 1 i) n
+            (+ result
+               (* (fk i)
+                 (cond ((= i 0) 1)
+                    ((= i n) 1)
+                    ((= (modulo i 2) 0) 2)
+                    ((= (modulo i 2) 1) 4)
+                )
+              )
+            )
+      )
+    )
+  )
+  (let ((h (/ (- b a) n)))
+    (/ (* h (iter (lambda (k) (f (+ a (* k h)))) 0 n 0)) 3)
+  )
+)
+
+;1.30
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+      result
+      (iter (next a) (+ a result))
+    )
+  )
+  (iter a 0)
+)
+
+;1.31
+(define (product-re a next b)
+  (define (odd? num) (= (modulo num 2) 1))
+  (define (iter a next b)
+    (if (> a b)
+      1
+      (if (odd? a)
+        (* (/ (+ a 1.0) (+ a 2.0)) (product (next a) next b))
+        (* (/ (+ a 2.0) (+ a 1.0)) (product (next a) next b))
+      )
+    )
+  )
+  (* 4 (iter a next b))
+)
+
+(define (product-it a next b)
+  (define (odd? num) (= (modulo num 2) 1))
+  (define (iter k result)
+    (if (> k b)
+      (* 4 result)
+      (iter (next k)
+            (if (odd? k)
+              (* (/ (+ k 1.0) (+ k 2.0)) result)
+              (* (/ (+ k 2.0) (+ k 1.0)) result)
+            )
+      )
+    )
+  )
+  (iter a 1)
+)
