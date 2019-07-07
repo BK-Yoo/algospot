adcb33ac21bd5147ce344396e3918b6942d7c32d
BK-Yoo <osehyum@gmail.com>
2019-07-07 10:37:34 +0900

    ch02 last part

--- lisp/ch02.scm
+++ lisp/ch02.scm
@@ -1122,3 +1122,72 @@
         num))))
 
 ; skip 2.86
+
+; 2.87
+(define (add-terms L1 L2)
+  (cond ((empty-termlist? L1) L2)
+        ((empty-termlist? L2) L1)
+        (else
+          (let ((t1 (first-term L1))
+                (t2 (first-term L2)))
+            (cond ((> (order t1) (order t2))
+                    (adjoin-term t1 (add-terms (rest-terms L1) L2)))
+                  ((< (order t1) (order t2))
+                    (adjoin-term t2 (add-terms (rest-terms L2) L1)))
+                  (else (adjoin-term
+                    (make-term (order t1)
+                               (add (coeff t1) (coeff t2))))
+                    (add-terms (rest-terms L1) (rest-terms L2)))
+            )
+          )
+        )
+  )
+)
+
+(define (mul-terms L1 L2)
+  (if (empty-termlist? L1)
+    (the-empty-termlist)
+    (add-terms (mul-terms-by-all-terms (first-term L1) L2)
+               (mul-terms (rest-terms L1) L2))))
+
+(define (mul-term-by-all-terms t1 L)
+ (if (empty-termlist? L)
+   (the-empty-termlist)
+   (let ((t2 (first-term L)))
+     (adjoin-term
+       (make-term (+ (order t1) (order t2))
+                  (* (coeff t1) (coeff t2)))
+       (mul-term-by-all-terms t1 (rest-terms L))
+     )
+   )
+ )
+)
+
+(define (install-polynomial-package)
+  (define (make-poly variable term-list)
+    (cons variable term-list))
+  
+  (define (variable p) (car p))
+  (define (term-list p) (cdr p))
+  (define (same-variable? v1 v2)
+  (define (add-poly p1 p2)
+    (if (same-variable? (variable p1) (variable p2))
+        (make-poly (variable p1)
+                   (add-terms (term-list p1) (term-list p2)))
+        (error "Polys not in same var: ADD-POLY" (list p1 p2))))
+  
+  (define (mul-poly p1 p2)
+    (if (same-variable? (variable p1) (variable p2))
+        (make-poly (variable p1)
+                   (mul-terms (term-list p1) (term-list p2)))
+        (error "Polys not in same var: ADD-POLY" (list p1 p2))))
+  
+     (and (variable? v1) (variable? v2) (eq? v1 v2)))
+  
+  (define (tag p) (attatch-tag 'polynomial p))
+  (put 'add '(polynomial polynomial)
+    (lambda (p1 p2) (tag (add-poly p1 p2))))
+  (put 'mul '(polynomial polynomial)
+    (lambda (p1 p2) (tag (mul-poly p1 p2))))
+  (put 'make 'polynomial
+    (lambda (var terms) (tag (make-poly var terms)))) 'done))
