1c2e752fa3fe5f07fb8066fa9179f1a859586302
BK-Yoo <osehyum@gmail.com>
2019-10-09 20:09:02 +0900

    Chapeter 3.3 done

--- lisp/ch03.scm
+++ lisp/ch03.scm
@@ -70,7 +70,7 @@
 ;3.5
 (define (random-in-range low high)
   (let ((range (- high low)))
-    (+ low (random (* 1 .0 range)))))
+    (+ low (random (* 1.0 range)))))
 
 (define (estimate-integral check x1 x2 y1 y2 trials)
   (define (iter passed tried)
@@ -227,7 +227,7 @@
 
 (has-cycle (make-cycle (list 'a 'b 'c)))
 
-; 3.18
+; 3.19
 
 ; memory increases linearly by one function call
 (define (has-cycle-fix x)
@@ -298,3 +298,269 @@
 (display (has-cycle-fix b))
 (display (has-cycle-fix c))
 (display (has-cycle-fix d))
+
+(define (adder a1 a2 sum)
+  (define (process-new-value)
+    (cond
+      ((and (has-value? a1) (has-value? a2))
+        (set-value! sum
+          (+ (get-value a1) (get-value a2))
+          me))
+      ((and (has-value? a1) (has-value? sum))
+        (set-value! a2
+          (- (get-value sum) (get-value a1))
+          me))
+      ((and (has-value? a2) (has-value? sum))
+        (set-value! a1
+          (- (get-value sum) (get-value a2))
+          me))))
+  (define (process-forget-value)
+    (forget-value! sum me)
+    (forget-value! a1 me)
+    (forget-value! a2 me)
+    (process-new-value))
+  (define (me request)
+    (cond
+      ((eq? request 'I-have-a-value) (process-new-value))
+      ((eq? request 'I-lost-my-value) (process-forget-value))
+      (else (error "Unknown request: ADDER" request))))
+  (connect a1 me)
+  (connect a2 me)
+  (connect sum me)
+  me)
+
+(define (multiplier m1 m2 product)
+  (define (process-new-value)
+    (cond
+      ((or (and (has-value? m1) (= (get-value m1) 0))
+         (and (has-value? m2) (= (get-value m2) 0)))
+        (set-value! product 0 me))
+      ((and (has-value? m1) (has-value? m2))
+        (set-value! product
+          (* (get-value m1) (get-value m2))
+          me))
+      ((and (has-value? product) (has-value? m1))
+        (set-value! m2
+          (/ (get-value product)
+            (get-value m1)) me))
+      ((and (has-value? product) (has-value? m2))
+        (set-value! m1
+          (/ (get-value product) (get-value m2))
+          me))))
+  (define (process-forget-value)
+    (forget-value! product me)
+    (forget-value! m1 me)
+    (forget-value! m2 me)
+    (process-new-value))
+  (define (me request)
+    (cond
+      ((eq? request 'I-have-a-value) (process-new-value))
+      ((eq? request 'I-lost-my-value) (process-forget-value))
+      (else (error "Unknown request: MULTIPLIER" request))))
+  (connect m1 me)
+  (connect m2 me)
+  (connect product me)
+  me)
+
+(define (constant value connector)
+  (define (me request)
+    (error "Unknown request: CONSTANT" request)) (connect connector me)
+  (set-value! connector value me)
+  me)
+
+(define (probe name connector)
+  (define (print-probe value)
+    (newline) (display "Probe: ") (display name)
+    (display " = ") (display value))
+  (define (process-new-value)
+    (print-probe (get-value connector)))
+  (define (process-forget-value) (print-probe "?"))
+  (define (me request)
+    (cond
+      ((eq? request 'I-have-a-value) (process-new-value))
+      ((eq? request 'I-lost-my-value) (process-forget-value))
+      (else (error "Unknown request: PROBE" request))))
+  (connect connector me)
+  me)
+
+; setter and retractor must be a one of constraint(adder, multiplier ..)
+; it means connector only changed by a constraint which set the value of that constraint
+(define (make-connector)
+  (let ((value false) (informant false) (constraints '()))
+    (define (set-my-value newval setter)
+      (cond
+        ((not (has-value? me))
+          (set! value newval)
+          (set! informant setter)
+          (for-each-except setter inform-about-value constraints))
+        ((not (= value newval))
+          (error "Contradiction" (list value newval)))
+        (else 'ignored)))
+    (define (forget-my-value retractor)
+      (if (eq? retractor informant)
+        (begin
+          (set! informant false)
+          (for-each-except retractor inform-about-no-value constraints))
+          'ignored))
+    (define (connect new-constraint)
+      (if (not (memq new-constraint constraints))
+        (set! constraints (cons new-constraint constraints)))
+      (if (has-value? me)
+        (inform-about-value new-constraint)
+          'done))
+    (define (me request)
+      (cond
+        ((eq? request 'has-value?) (if informant true false))
+        ((eq? request 'value) value)
+        ((eq? request 'set-value!) set-my-value)
+        ((eq? request 'forget) forget-my-value)
+        ((eq? request 'connect) connect)
+        ((eq? request 'source ) informant)
+        (else (error "Unknown operation: CONNECTOR" request))))
+    me))
+
+(define (for-each-except exception procedure list)
+  (define (loop items)
+    (cond ((null? items) 'done)
+      ((eq? (car items) exception) (loop (cdr items)))
+      (else (procedure (car items))
+        (loop (cdr items)))))
+  (loop list))
+
+(define (has-value? connector)
+  (connector 'has-value?))
+(define (get-value connector)
+  (connector 'value))
+(define (set-value! connector new-value informant)
+  ((connector 'set-value!) new-value informant))
+(define (forget-value! connector retractor)
+  ((connector 'forget) retractor))
+(define (connect connector new-constraint)
+  ((connector 'connect) new-constraint))
+(define (source connector)
+  (connector 'source))
+
+(define (inform-about-value constraint)
+  (constraint 'I-have-a-value))
+(define (inform-about-no-value constraint)
+  (constraint 'I-lost-my-value))
+
+(define (celsius-fahrenheit-converter c f)
+  (let ((u (make-connector))
+         (v (make-connector))
+         (w (make-connector))
+         (x (make-connector))
+         (y (make-connector)))
+    (multiplier c w u)
+    (multiplier v x u)
+    (adder v y f)
+    (constant 9 w)
+    (constant 5 x)
+    (constant 32 y)
+      'ok))
+
+(define C (make-connector))
+(define F (make-connector))
+(celsius-fahrenheit-converter C F)
+
+(probe "Celsius temp" C)
+(probe "Fahrenheit temp" F)
+
+
+; 3.33
+; a + b = u -- u = c * 2
+(define (averager a b c)
+  (let ((u (make-connector))
+         (x (make-connector)))
+    (adder a b u)
+    (multiplier c x u)
+    (constant 2 x)
+    'ok))
+
+(define a (make-connector))
+(define b (make-connector))
+(define c (make-connector))
+
+(averager a b c)
+
+(probe "value A" a)
+(probe "value B" b)
+(probe "value C" c)
+
+(set-value! a 20 'user)
+(set-value! b 30 'user)
+
+; 3.34
+(define (wrong-squarer a b)
+  (multiplier a a b))
+
+; in multiplier,
+; set value to e doesn't trigger the event
+; because we need at least 2 connetors have value(in this case only e)
+
+; 3.35
+(define (squarer a b)
+  (define (process-new-value)
+    (if (has-value? b)
+      (if (< (get-value b) 0)
+        (error "square less than 0: SQUARER")
+        (set-value! a (sqrt (get-value b)) me))
+      (if (has-value? a)
+        (set-value! b (* (get-value a) (get-value a)) me))))
+  (define (process-forget-value)
+    (forget-value! a me)
+    (forget-value! b me)
+    (process-new-value))
+  (define (me request)
+    (cond
+      ((eq? request 'I-have-a-value) (process-new-value))
+      ((eq? request 'I-lost-my-value) (process-forget-value))
+      (else (error "Unknown request: SQUARER" request))))
+  (connect a me)
+  (connect b me)
+  me)
+
+(define d (make-connector))
+(define e (make-connector))
+
+(squarer d e)
+(probe "value d" d)
+(probe "value e" e)
+
+(set-value! e 50 'user)
+
+; 3.36 skip
+
+; 3.37
+
+(define (celsius-fahrenheit-converter2 c)
+  (c+ (c* (c/ (cv 9) (cv 5)) c)
+    (cv 32)))
+
+(define (c+ a b)
+  (let ((z (make-connector)))
+    (adder a b z)
+    z))
+
+(define (c* a b)
+  (let ((z (make-connector)))
+    (multiplier a b z)
+    z))
+
+(define (c/ a b)
+  (let ((z (make-connector)))
+    (multiplier b z a)
+    z))
+
+(define (cv c)
+  (let ((z (make-connector)))
+    (constant c z)
+    z))
+
+(define X (make-connector))
+(define Y (celsius-fahrenheit-converter2 X))
+
+(probe "X: " X)
+(probe "Y: " Y)
+
+(set-value! X 30 'user)
