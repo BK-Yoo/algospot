f0b23b5329f91e1e44cf9750e6345ddb1243fd44
BK-Yoo <osehyum@gmail.com>
2019-10-28 12:28:44 +0900

    power series

--- lisp/ch03-5.scm
+++ lisp/ch03-5.scm
@@ -248,3 +248,35 @@
     1.0
     (stream-map (lambda (guess) (sqrt-improve guess x))
                      (sqrt-stream2 x))))
+
+; 3.64
+(define (stream-limit stream tol)
+  (let ((prev (stream-ref stream 0))
+         (next (stream-ref stream 1)))
+    (if (< (abs (- next prev)) tol)
+      next
+      (stream-limit (stream-cdr stream) tol))))
+
+; 3.65
+(define (n-log-stream n)
+  (cons-stream (/ 1.0 n)
+    (stream-map - (n-log-stream (+ n 1)))))
+
+(define n-log
+  (partial-sum2 (n-log-stream 1)))
+
+(define (euler-transform s)
+  (let ((s0 (stream-ref s 0))
+        (s1 (stream-ref s 1))
+        (s2 (stream-ref s 2)))
+    (cons-stream (- s2 (/ (square (- s2 s1))
+                         (+ s0 (* -2 s1) s2)))
+      (euler-transform (stream-cdr s)))))
+
+(define (make-tableau transform s)
+  (cons-stream s (make-tableau transform (transform s))))
+
+(define (accelerated-sequence transform s)
+  (stream-map stream-car (make-tableau transform s)))
+
+(define acc-ln2 (accelerated-sequence euler-transform n-log))
